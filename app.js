c9c111c7ba62c7d41b8f12f3983a6b5f8caa1303
seth808rose <69048387+seth808rose@users.noreply.github.com>
2021-09-15 12:14:38 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// selectors
+const formInput = document.querySelector(".form-input");
+const formButton = document.querySelector(".form-button");
+const formList = document.querySelector(".todo-list");
+
+// Event listeners
+
+// functions
